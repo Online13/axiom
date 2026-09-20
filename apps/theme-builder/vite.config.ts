f6766c6b34dc62f5e8130f6ad5f8dcf5260a7108
RATIARIVELO Nekena Rayane <rratiarivelo@gmail.com>
2026-09-20 07:26:47 +0300

    feat: update theme-builder configuration with Cloudflare plugin and Wrangler support

--- apps/theme-builder/vite.config.ts
+++ apps/theme-builder/vite.config.ts
@@ -1,15 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-
 	const env = loadEnv(mode, process.cwd(), "");
 
 	return {
-		plugins: [react()],
+		plugins: [react(), cloudflare()],
 		server: {
-			port: Number(env.PORT) || 4323
-		}
+			port: Number(env.PORT) || 4323,
+		},
 	};
 });
